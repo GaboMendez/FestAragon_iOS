6d27eea5010c54069a5d2cbfac9afe64bb2188f4
Gabriel Mendez Reyes <gabriel.mendez0@hotmail.com>
2026-01-14 17:38:13 +0100

    Add initial project files and structure for FestAragon iOS app

---
+++ FestAragon/FestAragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestAragonApp.swift
+//  FestAragon
+//
+//  Created by Gabriel Mendez Reyes on 14/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FestAragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
